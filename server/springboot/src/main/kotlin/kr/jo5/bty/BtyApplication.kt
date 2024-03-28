60b3773461cc076535745a1316b85fe9a5794ced
johyun <zimkjh@naver.com>
2024-03-28 22:58:56 +0900

    Init spring boot server

---
+++ server/springboot/src/main/kotlin/kr/jo5/bty/BtyApplication.kt
@@ -0,0 +1,11 @@
+package kr.jo5.bty
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BtyApplication
+
+fun main(args: Array<String>) {
+	runApplication<BtyApplication>(*args)
+}
---
+++ server/springboot/src/test/kotlin/kr/jo5/bty/BtyApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.jo5.bty
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BtyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
